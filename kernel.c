b6865744ad02ad3f007b6ba1fa2e1b364e63daec
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-01 21:20:38 +0000

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,23 @@
+#include "base.h"
+
+void kernel_main()
+{
+        int n=0;
+        int xx;
+        char Yellow=14;
+        int i=640/2;
+        int ii=0;
+        char c=Yellow;
+        char Black=0;
+	cls();
+        gputs(0,10," hello world .....");
+        redraw();
+        while(1){}
+
+        
+        
+	
+	
+}
+
+
